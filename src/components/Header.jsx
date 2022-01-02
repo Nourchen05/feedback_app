9983b524289e0f889256ad9b9a7658f95ec55be8
Nourchen05 <noamennourchen5@gmail.com>
2022-01-02 11:24:00 +0100

    defaultProps propTypes style in component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const Header = ({ text }) => {
+const Header = ({ text, bgColor, textColor }) => {
   return (
-    <header>
+    <header style={{ backgroundColor: bgColor, color: textColor }}>
       <div className="container">
         <h2>{text}</h2>
       </div>
     </header>
   );
+};
+
+Header.defaultProps = {
+  text: "Feedback App",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+  text: propTypes.string,
+  bgColor: propType.string,
+  textColor: propType.string,
 };
 
 export default Header;
