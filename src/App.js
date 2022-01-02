c6ae121a489894dc67fa178165d1d90786454ddf
Nourchen05 <noamennourchen5@gmail.com>
2022-01-02 11:02:18 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
-import React, { createElement } from "react";
+import React from "react";
+import Header from "./components/Header";
 
-function Hello() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "comment one" },
-    { id: 2, text: "comment two" },
-    { id: 3, text: "comment three" },
-  ];
-
-  //conditionale JSX
-  const loading = false;
-  const showComments = true;
-  if (loading) return <h1>It is loading ...</h1>;
-
+const App = ({ text }) => {
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {/*Ternary conditional operator*/}
-      {showComments ? (
-        <div className="comments">
-          <p>There are {comments.length} comments</p>
-          {comments.map((el, index) => (
-            <li key={index}>{el.text}</li>
-          ))}
-        </div>
-      ) : null}
-
-      {/* second method*/}
-      {/**
-       * {showComments && (
-        <div className="comments">
-          <p>There are {comments.length} comments</p>
-          {comments.map((el, index) => (
-            <li key={index}>{el.text}</li>
-          ))}
-        </div>
-      ) }
-       * 
-       */}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+        <h2>{text}</h2>
+      </div>
+    </>
   );
-}
+};
 
-export default Hello;
+Header.defaultProps = {
+  text: "Feedback UI",
+};
+
+export default App;
